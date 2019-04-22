276d8216ef92fe52336a7e99a9d863484fcedf67
thecoldstone <44757028+thecoldstone@users.noreply.github.com>
2019-04-22 16:20:17 +0200

    Example2 is a copy of GUI, but use Math.py library for calculation

---
+++ src/Math.py
@@ -0,0 +1,26 @@
+import math
+
+class Math(object):
+    def Add(a, b):
+        return a + b
+
+    def Sub(a, b):
+        return a - b
+
+    def Abs(a):
+        return math.fabs(a)
+
+    def Divison(a, b):
+        return a / b
+
+    def Multiply(a, b):
+        return a * b
+
+    def Factorial(a):
+        return math.factorial(a)
+
+    def Power(a, b):
+        return math.pow(a, b)
+
+    def Square(a, b):
+        return math.pow(a,1./b)
